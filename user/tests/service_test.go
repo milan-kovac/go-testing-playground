bf155138d9bf8ee86530a35c6935cb130fbfdd70
Milan Kovac <113798238+milan-kovac@users.noreply.github.com>
2025-05-20 21:26:07 +0200

    Tests for service 🧪

--- user/tests/service_test.go
+++ user/tests/service_test.go
@@ -3,6 +3,7 @@ package user_test
 import (
 	"errors"
 	"testing"
+	"time"
 
 	"github.com/milan-kovac/user"
 	"github.com/stretchr/testify/assert"
@@ -13,15 +14,18 @@ func TestCreateUser(t *testing.T) {
 	mockRepo := new(MockUserRepository)
 	service := user.NewUserService(mockRepo)
 
-	t.Run("successfully creates user", func(t *testing.T) {
+	t.Run("should create user successfully", func(t *testing.T) {
 		req := user.CreateUserRequest{
-			FirstName: "Milan",
-			LastName:  "Kovač",
+			FirstName: "James",
+			LastName:  "Smith",
 		}
 
 		expectedUser := &user.User{
-			FirstName: "Milan",
-			LastName:  "Kovač",
+			ID:        1,
+			FirstName: "James",
+			LastName:  "Smith",
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
 		}
 
 		mockRepo.On("Create", mock.AnythingOfType("*user.User")).Return(expectedUser, nil).Once()
@@ -35,7 +39,7 @@ func TestCreateUser(t *testing.T) {
 		mockRepo.AssertExpectations(t)
 	})
 
-	t.Run("fails to create user when repo returns error", func(t *testing.T) {
+	t.Run("should fail to create user when repo returns error", func(t *testing.T) {
 		req := user.CreateUserRequest{
 			FirstName: "Error",
 			LastName:  "Case",
@@ -46,6 +50,74 @@ func TestCreateUser(t *testing.T) {
 		createdUser, err := service.Create(req)
 
 		assert.Nil(t, createdUser)
+		assert.EqualError(t, err, "db error")
+
+		mockRepo.AssertExpectations(t)
+	})
+
+	t.Run("should return all users successfully", func(t *testing.T) {
+		expectedUsers := []user.User{
+			{
+				ID:        1,
+				FirstName: "Michael",
+				LastName:  "Johnson",
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+			},
+			{
+				ID:        2,
+				FirstName: "Emily",
+				LastName:  "Davis",
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+			},
+		}
+
+		mockRepo.On("GetAll").Return(expectedUsers, nil).Once()
+
+		users, err := service.GetAll()
+
+		assert.NoError(t, err)
+		assert.Equal(t, expectedUsers, users)
+
+		mockRepo.AssertExpectations(t)
+	})
+
+	t.Run("should return error when failing to get all users", func(t *testing.T) {
+		mockRepo.On("GetAll").Return([]user.User(nil), errors.New("db error")).Once()
+
+		users, err := service.GetAll()
+
+		assert.Nil(t, users)
+		assert.EqualError(t, err, "db error")
+
+		mockRepo.AssertExpectations(t)
+	})
+
+	t.Run("should return user successfully", func(t *testing.T) {
+		expectedUser := user.User{
+			ID:        1,
+			FirstName: "Michael",
+			LastName:  "Johnson",
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		}
+		mockRepo.On("Get", int(expectedUser.ID)).Return(&expectedUser, nil).Once()
+
+		user, err := service.Get(int(expectedUser.ID))
+
+		assert.NoError(t, err)
+		assert.Equal(t, expectedUser, *user)
+
+		mockRepo.AssertExpectations(t)
+	})
+
+	t.Run("should return error when failing to get user", func(t *testing.T) {
+		mockRepo.On("Get", 1234).Return((*user.User)(nil), errors.New("db error")).Once()
+
+		user, err := service.Get(1234)
+
+		assert.Nil(t, user)
 		assert.EqualError(t, err, "db error")
 
 		mockRepo.AssertExpectations(t)
